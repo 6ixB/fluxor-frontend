d21bd62f2803ea94b1b56f3cffa085e196d30828
6ixB <124480418+6ixB@users.noreply.github.com>
2025-11-15 23:10:45 +0700

    refactor: increase drone target size

--- src/components/body/viewport/viewport-animation-player/viewport-animation-target.tsx
+++ src/components/body/viewport/viewport-animation-player/viewport-animation-target.tsx
@@ -1,7 +1,7 @@
 const ViewPortAnimationTarget: React.FC = () => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.001, 0]}>
-      <circleGeometry args={[0.2, 32]} />
+      <circleGeometry args={[0.5, 32]} />
       <meshBasicMaterial color="#ff0000" opacity={0.8} transparent />
     </mesh>
   );
