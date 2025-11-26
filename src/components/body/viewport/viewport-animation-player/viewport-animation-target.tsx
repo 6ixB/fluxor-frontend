2d3a5da3c212621190e889c9fa3516106b6134b5
6ixB <124480418+6ixB@users.noreply.github.com>
2025-11-26 11:11:32 +0700

    refactor: change drone delivery target opacity

--- src/components/body/viewport/viewport-animation-player/viewport-animation-target.tsx
+++ src/components/body/viewport/viewport-animation-player/viewport-animation-target.tsx
@@ -2,7 +2,7 @@ const ViewPortAnimationTarget: React.FC = () => {
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.001, 0]}>
       <circleGeometry args={[0.5, 32]} />
-      <meshBasicMaterial color="#ff0000" opacity={0.8} transparent />
+      <meshBasicMaterial color="#ff0000" opacity={0.5} transparent />
     </mesh>
   );
 };
