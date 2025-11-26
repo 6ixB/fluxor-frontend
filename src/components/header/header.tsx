5c17dbcd1f60ef7f333525e6bf9d5209a583fe40
6ixB <124480418+6ixB@users.noreply.github.com>
2025-11-26 22:29:50 +0700

    feat: show app icon when sidebar is closed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,22 @@
 import { InformationDialog } from "@/components/header/information-dialog";
 import { ThemeModeToggle } from "@/components/header/theme-mode-toggle";
-import { SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { DroneIcon } from "lucide-react";
 
 const Header: React.FC = () => {
+  const { open } = useSidebar();
+
   return (
     <header className="bg-background w-full border-b border-neutral-200 px-4 py-2 dark:border-neutral-800">
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-4">
+        <div className="flex items-center gap-x-2">
           <SidebarTrigger />
+          {!open && (
+            <div className="flex items-center gap-x-2 select-none">
+              <DroneIcon className="size-[1.2rem]" />
+              <span className="text-lg font-semibold">Fluxor</span>
+            </div>
+          )}
         </div>
         <div className="flex items-center justify-end gap-x-2">
           <InformationDialog />
