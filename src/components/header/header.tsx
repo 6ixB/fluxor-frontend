04c496b097120068005166b242e16b60b69f2ae6
6ixB <124480418+6ixB@users.noreply.github.com>
2025-11-27 11:25:16 +0700

    feat: add separator between toggle sidebar open and app icon

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import { InformationDialog } from "@/components/header/information-dialog";
 import { ThemeModeToggle } from "@/components/header/theme-mode-toggle";
 import { SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 import { DroneIcon } from "lucide-react";
 
 const Header: React.FC = () => {
@@ -9,13 +10,16 @@ const Header: React.FC = () => {
   return (
     <header className="bg-background w-full border-b border-neutral-200 px-4 py-2 dark:border-neutral-800">
       <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-2">
+        <div className="flex h-6 items-center space-x-2 text-sm">
           <SidebarTrigger />
           {!open && (
-            <div className="flex items-center gap-x-2 select-none">
-              <DroneIcon className="size-[1.2rem]" />
-              <span className="text-lg font-semibold">Fluxor</span>
-            </div>
+            <>
+              <Separator orientation="vertical" />
+              <div className="flex items-center gap-x-2 select-none">
+                <DroneIcon className="size-[1.2rem]" />
+                <span className="text-lg font-semibold">Fluxor</span>
+              </div>
+            </>
           )}
         </div>
         <div className="flex items-center justify-end gap-x-2">
